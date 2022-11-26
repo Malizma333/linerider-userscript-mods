1236c8cd6ebcb3ac094eca1fd1c34dc15c22906e
Malizma333 <63310489+Malizma333@users.noreply.github.com>
2022-11-26 01:21:54 -0600

    download url

--- mods/line-rider-maze-mod.user.js
+++ mods/line-rider-maze-mod.user.js
@@ -10,6 +10,7 @@
 // @match        https://*.official-linerider.com/*
 // @match        http://localhost:8000/*
 // @grant        none
+// @downloadURL  https://github.com/Malizma333/linerider-userscript-mods/raw/master/mods/line-rider-maze-mod.user.js
 // ==/UserScript==
 
 const updateLines = (linesToRemove, linesToAdd, name) => ({
