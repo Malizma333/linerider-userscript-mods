d81ef109e8f71e48a45e28d60b3f9820c99a3185
Malizma333 <malizma333@gmail.com>
2023-04-04 19:41:58 -0500

    Update line-rider-command-editor.user.js

--- mods/line-rider-command-editor.user.js
+++ mods/line-rider-command-editor.user.js
@@ -8,11 +8,11 @@
 // @match        https://*.official-linerider.com/*
 // @match        http://localhost:8000/*
 // @grant        none
-// @require      https://github.com/Malizma333/line-rider-command-editor-userscript/raw/master/ProjectFiles/commandEditor.js
-// @require      https://github.com/Malizma333/line-rider-command-editor-userscript/raw/master/ProjectFiles/renderFunctions.js
 // @require      https://github.com/Malizma333/line-rider-command-editor-userscript/raw/master/ProjectFiles/styleConstants.js
 // @require      https://github.com/Malizma333/line-rider-command-editor-userscript/raw/master/ProjectFiles/valueConstants.js
 // @require      https://github.com/Malizma333/line-rider-command-editor-userscript/raw/master/ProjectFiles/windowConstants.js
+// @require      https://github.com/Malizma333/line-rider-command-editor-userscript/raw/master/ProjectFiles/renderFunctions.js
+// @require      https://github.com/Malizma333/line-rider-command-editor-userscript/raw/master/ProjectFiles/commandEditor.js
 // @require      https://github.com/Malizma333/line-rider-command-editor-userscript/raw/master/ProjectFiles/main.js
 // @downloadURL  https://github.com/Malizma333/linerider-userscript-mods/raw/master/mods/line-rider-command-editor.user.js
 // @updateURL    https://github.com/Malizma333/linerider-userscript-mods/raw/master/mods/line-rider-command-editor.user.js
