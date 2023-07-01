21ed196f9a6dafb4cd492d20e770ac19cfddf1bc
Malizma333 <malizma333@gmail.com>
2023-07-01 18:02:54 -0500

    Update line-rider-tenpc-generator-mod.js

--- mods/line-rider-tenpc-generator-mod.js
+++ mods/line-rider-tenpc-generator-mod.js
@@ -11,7 +11,8 @@
 // @match        http://localhost:8000/*
 // @match        https://*.surge.sh/*
 // @grant        none
-
+// @downloadURL  https://github.com/Malizma333/linerider-userscript-mods/raw/master/mods/line-rider-tenpc-generator-mod.js
+// @updateURL    https://github.com/Malizma333/linerider-userscript-mods/raw/master/mods/line-rider-tenpc-generator-mod.js
 // ==/UserScript==
 
 const updateLines = (linesToRemove, linesToAdd, name) => ({
