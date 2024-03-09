5b1e961afc93c4e80a9e80f04120898973933ba5
Tobias Bessler <malizma333@gmail.com>
2024-03-09 16:44:37 -0600

    Update line-rider-svg-exporter.user.js
    version fix

--- mods/line-rider-svg-exporter.user.js
+++ mods/line-rider-svg-exporter.user.js
@@ -4,7 +4,7 @@
 // @namespace    https://www.linerider.com/
 // @author       Malizma
 // @description  Generates an svg file from a selection of lines
-// @version      1.2.0
+// @version      1.0.0
 // @icon         https://www.linerider.com/favicon.ico
 
 // @match        https://www.linerider.com/*
