19323e0dc110190491d8d8b9bdc4c311a87bbce8
Tobias Bessler <malizma333@gmail.com>
2024-04-12 15:19:25 -0500

    Update line-rider-command-editor.user.js

--- mods/line-rider-command-editor.user.js
+++ mods/line-rider-command-editor.user.js
@@ -1,25 +1,14 @@
 // ==UserScript==
 // @name         Command Editor
 // @author       Malizma
-// @description  Adds UI to certain commands in linerider.com
+// @description  Adds UI to API commands in linerider.com
 // @namespace    https://www.linerider.com/
-// @version      1.1.0
+// @version      2.0.0
 // @icon         https://www.linerider.com/favicon.ico
 // @match        https://www.linerider.com/*
 // @match        https://*.official-linerider.com/*
 // @match        http://localhost:*/*
 // @match        https://*.surge.sh/*
 // @grant        none
-// @require      https://github.com/Malizma333/line-rider-command-editor-userscript/raw/master/ProjectFiles/styleConstants.js
-// @require      https://github.com/Malizma333/line-rider-command-editor-userscript/raw/master/ProjectFiles/valueConstants.js
-// @require      https://github.com/Malizma333/line-rider-command-editor-userscript/raw/master/ProjectFiles/windowConstants.js
-// @require      https://github.com/Malizma333/line-rider-command-editor-userscript/raw/master/ProjectFiles/validateFunctions.js
-// @require      https://github.com/Malizma333/line-rider-command-editor-userscript/raw/master/ProjectFiles/parseFunctions.js
-// @require      https://github.com/Malizma333/line-rider-command-editor-userscript/raw/master/ProjectFiles/renderFunctions.js
-// @require      https://github.com/Malizma333/line-rider-command-editor-userscript/raw/master/ProjectFiles/commandEditor.js
-// @require      https://github.com/Malizma333/line-rider-command-editor-userscript/raw/master/ProjectFiles/main.js
-// @downloadURL  https://github.com/Malizma333/linerider-userscript-mods/raw/master/mods/line-rider-command-editor.user.js
-// @updateURL    https://github.com/Malizma333/linerider-userscript-mods/raw/master/mods/line-rider-command-editor.user.js
-// @homepageURL  https://github.com/Malizma333/line-rider-command-editor-userscript
-// @supportURL   https://github.com/Malizma333/line-rider-command-editor-userscript/issues
+// @require      https://github.com/Malizma333/line-rider-command-editor-userscript/raw/master/command-editor.min.js
 // ==/UserScript==
