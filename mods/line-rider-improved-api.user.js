3d54923e3d5bc4170666248f4aebd0a1921970b4
Tobias Bessler <malizma333@gmail.com>
2024-08-12 09:16:56 -0500

    Update line-rider-improved-api.user.js
    - Changed look back from dropdown for regular mods
    - Removed settings and set flexible width and height

--- mods/line-rider-improved-api.user.js
+++ mods/line-rider-improved-api.user.js
@@ -4,7 +4,7 @@
 // @namespace    https://www.linerider.com/
 // @author       Malizma
 // @description  Container for linerider.com mods
-// @version      1.4.2
+// @version      1.5.0
 // @icon         https://www.linerider.com/favicon.ico
 
 // @match        https://www.linerider.com/*
@@ -23,8 +23,8 @@
 // jshint esversion: 6
 
 const setTool = (tool) => ({
-  type: "SET_TOOL",
-  payload: tool
+    type: "SET_TOOL",
+    payload: tool
 });
 
 const getActiveTool = state => state.selectedTool;
@@ -32,235 +32,226 @@ const getWindowFocused = state => state.views.Main;
 const getPlayerRunning = state => state.player.running;
 
 function main () {
-  window.V2 = window.V2 || window.store.getState().simulator.engine.engine.state.startPoint.constructor;
+    window.V2 = window.V2 || window.store.getState().simulator.engine.engine.state.startPoint.constructor;
 
-  const {
-    React,
-    ReactDOM,
-    store
-  } = window;
+    const {
+        React,
+        ReactDOM,
+        store
+    } = window;
 
-  const e = React.createElement;
-  var playerRunning = getPlayerRunning(store.getState());
-  var windowFocused = getWindowFocused(store.getState());
+    const e = React.createElement;
+    var playerRunning = getPlayerRunning(store.getState());
+    var windowFocused = getWindowFocused(store.getState());
 
-  const rootStyle = {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "flex-start",
-    textAlign: "left",
-    transition: "opacity 225ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
-    width: "100%"
-  };
+    const rootStyle = {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "flex-start",
+        textAlign: "left",
+        transition: "opacity 225ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
+        width: "100%"
+    };
 
-  const boxStyle = {
-    display: "flex",
-    flexDirection: "column-reverse",
-    padding: 8,
-    width: "100%"
-  };
+    const boxStyle = {
+        display: "flex",
+        flexDirection: "column-reverse",
+        padding: 8,
+        width: "100%"
+    };
 
-  store.subscribe(() => {
-    playerRunning = getPlayerRunning(store.getState());
-    windowFocused = getWindowFocused(store.getState());
+    store.subscribe(() => {
+        playerRunning = getPlayerRunning(store.getState());
+        windowFocused = getWindowFocused(store.getState());
 
-    let shouldBeVisible = !playerRunning && windowFocused;
+        let shouldBeVisible = !playerRunning && windowFocused;
 
-    toolContainer.style.opacity = shouldBeVisible ? 1 : 0;
-    toolContainer.style.pointerEvents = shouldBeVisible ? null : "none";
+        toolContainer.style.opacity = shouldBeVisible ? 1 : 0;
+        toolContainer.style.pointerEvents = shouldBeVisible ? null : "none";
 
-    settingsContainer.style.opacity = shouldBeVisible ? 1 : 0;
-    settingsContainer.style.pointerEvents = shouldBeVisible ? null : "none";
-  });
+        settingsContainer.style.opacity = shouldBeVisible ? 1 : 0;
+        settingsContainer.style.pointerEvents = shouldBeVisible ? null : "none";
+    });
 
-  class CustomToolsContainer extends React.Component {
-    constructor () {
-      super();
+    class CustomToolsContainer extends React.Component {
+        constructor () {
+            super();
 
-      this.state = {
-        activeTool: getActiveTool(store.getState()),
-        customTools: {}
-      };
+            this.state = {
+                activeTool: getActiveTool(store.getState()),
+                customTools: {}
+            };
 
-      store.subscribe(() => {
-        const activeTool = getActiveTool(store.getState());
-        if (this.state.activeTool !== activeTool) {
-          let activeCustomTool = this.state.customTools[this.state.activeTool];
-          if (activeCustomTool && activeCustomTool.onDetach) {
-            activeCustomTool.onDetach();
-          }
-          this.setState({ activeTool });
-        }
-      });
-    }
-
-    componentDidMount () {
-      let containerAssigned = false;
-      window.registerCustomTool = (toolName, tool, component, onDetach) => {
-        console.info("Registering custom tool", toolName);
-
-        window.Tools[toolName] = tool;
-
-        this.setState((prevState) => ({
-          customTools: {
-            ...prevState.customTools,
-            [toolName]: { component, onDetach }
-          }
-        }));
-
-        if (onDetach) {
-          this.customToolsDestructors[toolName] = onDetach;
+            store.subscribe(() => {
+                const activeTool = getActiveTool(store.getState());
+                if (this.state.activeTool !== activeTool) {
+                    let activeCustomTool = this.state.customTools[this.state.activeTool];
+                    if (activeCustomTool && activeCustomTool.onDetach) {
+                        activeCustomTool.onDetach();
+                    }
+                    this.setState({ activeTool });
+                }
+            });
         }
 
-        if (!containerAssigned) {
-          containerAssigned = true;
-          Object.assign(toolContainer.style, {
-            position: "fixed",
-            width : "200px",
-            height: "16%",
-            overflowY: "auto",
-            overflowX: "hidden",
-            top: "8px",
-            right: "250px",
-            border: "1px solid black",
-            backgroundColor: "#ffffff",
-            opacity: 0,
-            pointerEvents: "none"
-          });
-        }
-      };
-    }
+        componentDidMount () {
+            let containerAssigned = false;
+            window.registerCustomTool = (toolName, tool, component, onDetach) => {
+                console.info("Registering custom tool", toolName);
 
-    render () {
-      const activeCustomTool = this.state.customTools[this.state.activeTool];
+                window.Tools[toolName] = tool;
 
-      return e("div", { style: rootStyle },
-        Object.keys(this.state.customTools).length > 0 && e("div", null,
-          e("select", {
-            style: { textAlign: "center", width : "200px" },
-            maxMenuHeight : 100,
-            value: this.state.activeTool,
-            onChange: e => {
-              store.dispatch(setTool(e.target.value));
-            } },
-          e("option", { value: "PENCIL_TOOL" }, "- Select Tool -"),
-          ...Object.keys(this.state.customTools).map(toolName =>
-            e("option", { value: toolName }, toolName)
-          ))
-        ),
-        activeCustomTool && activeCustomTool.component && e("div", { style: boxStyle }, e(activeCustomTool.component))
-      );
-    }
-  }
+                this.setState((prevState) => ({
+                    customTools: {
+                        ...prevState.customTools,
+                        [toolName]: { component, onDetach }
+                    }
+                }));
 
-  const toolContainer = document.createElement("div");
+                if (onDetach) {
+                    this.customToolsDestructors[toolName] = onDetach;
+                }
 
-  document.getElementById("content").appendChild(toolContainer);
-
-  ReactDOM.render(
-    e(CustomToolsContainer),
-    toolContainer
-  );
-
-  class CustomSettingsContainer extends React.Component {
-    constructor () {
-      super();
-
-      this.state = {
-        activeSetting: null,
-        customSettings: []
-      };
-    }
-
-    componentDidMount () {
-      let containerAssigned = false;
-
-      window.registerCustomSetting = (component) => {
-        console.info("Registering custom setting", component.name);
-        this.setState((prevState) => ({
-          customSettings: [ ...prevState.customSettings, component ]
-        }));
-
-        if (!containerAssigned) {
-          containerAssigned = true;
-          Object.assign(settingsContainer.style, {
-            position: "fixed",
-            width : "200px",
-            height: "150px",
-            overflowY: "auto",
-            overflowX: "hidden",
-            bottom: "15%",
-            right: "8px",
-            border: "1px solid black",
-            backgroundColor: "#ffffff",
-            opacity: 0,
-            pointerEvents: "none"
-          });
-        }
-      };
-
-      if (typeof window.onCustomToolsApiReady === "function") {
-        window.onCustomToolsApiReady();
-      }
-    }
-
-    createDisplayName (className) {
-      if (className.length == 0) return "Unnamed Mod";
-
-      let finalName = [ className[0].toUpperCase() ];
-
-      for (let i = 1; i < className.length; i++) {
-        let character = className[i];
-
-        if (character == character.toUpperCase()) {
-          finalName.push("");
+                if (!containerAssigned) {
+                    containerAssigned = true;
+                    Object.assign(toolContainer.style, {
+                        position: "fixed",
+                        width : "200px",
+                        height: "16%",
+                        overflowY: "auto",
+                        overflowX: "hidden",
+                        top: "8px",
+                        right: "250px",
+                        border: "1px solid black",
+                        backgroundColor: "#ffffff",
+                        opacity: 0,
+                        pointerEvents: "none"
+                    });
+                }
+            };
         }
 
-        finalName[finalName.length - 1] += character;
-      }
+        render () {
+            const activeCustomTool = this.state.customTools[this.state.activeTool];
 
-      finalName = finalName.filter(e => e.toUpperCase() !== "COMPONENT");
-
-      return finalName.join(" ");
+            return e(
+                "div",
+                { style: rootStyle },
+                Object.keys(this.state.customTools).length > 0 && e(
+                    "div",
+                    null,
+                    e(
+                        "select",
+                        {
+                            style: { textAlign: "center", width : "200px" },
+                            maxMenuHeight : 100,
+                            value: this.state.activeTool,
+                            onChange: e => store.dispatch(setTool(e.target.value))
+                        },
+                        e("option", { value: "PENCIL_TOOL" }, "- Select Tool -"),
+                        ...Object.keys(this.state.customTools).map(
+                            toolName => e(
+                                "option",
+                                { value: toolName },
+                                toolName
+                            )
+                        )
+                    )
+                ),
+                activeCustomTool && activeCustomTool.component && e(
+                    "div",
+                    { style: boxStyle },
+                    e(activeCustomTool.component)
+                )
+            );
+        }
     }
 
-    render () {
-      const activeSetting = this.state.customSettings[this.state.activeSetting];
+    const toolContainer = document.createElement("div");
 
-      this.state.customSettings.sort(function (modA, modB) {
-        var modAName = modA.name.toUpperCase();
-        var modBName = modB.name.toUpperCase();
-        if (modAName == "SETTINGS") return -1;
-        if (modBName == "SETTINGS") return 1;
-        return (modAName < modBName) ? -1 : (modAName > modBName) ? 1 : 0;
-      });
+    document.getElementById("content").appendChild(toolContainer);
 
-      return e("div", { style: rootStyle },
-        this.state.customSettings.length > 1 && e("div", { style: { width: "100%" } },
-          e("select", {
-            style: { textAlign: "center", width: "100%" },
-            value: this.state.activeSetting,
-            onChange: e => this.setState({ activeSetting : e.target.value }) },
-          e("option", { value: null }, "- Select Mod -"),
-          this.state.customSettings.map((option, index) => (
-            e("option", { value: index }, this.createDisplayName(option.name))
-          ))
-          )
-        ),
-        activeSetting && e("div", { style: boxStyle }, e(activeSetting))
-      );
+    ReactDOM.render(
+        e(CustomToolsContainer),
+        toolContainer
+    );
+
+    class CustomSettingsContainer extends React.Component {
+        constructor () {
+            super();
+
+            this.state = {
+                customSettings: []
+            };
+        }
+
+        componentDidMount () {
+            let containerAssigned = false;
+
+            window.registerCustomSetting = (component) => {
+                console.info("Registering custom setting", component.name);
+                this.setState((prevState) => ({
+                    customSettings: [ ...prevState.customSettings, component ]
+                }));
+
+                if (!containerAssigned) {
+                    containerAssigned = true;
+                    Object.assign(settingsContainer.style, {
+                        position: "fixed",
+                        width: "20vw",
+                        height: "25vh",
+                        overflowY: "auto",
+                        overflowX: "hidden",
+                        bottom: "15%",
+                        right: "8px",
+                        border: "1px solid black",
+                        backgroundColor: "#ffffff",
+                        opacity: 0,
+                        pointerEvents: "none"
+                    });
+                }
+            };
+
+            if (typeof window.onCustomToolsApiReady === "function") {
+                window.onCustomToolsApiReady();
+            }
+        }
+
+        render () {
+            this.state.customSettings.sort(function (modA, modB) {
+                var modAName = modA.name.toUpperCase();
+                var modBName = modB.name.toUpperCase();
+                if (modAName == "SETTINGS") return -1;
+                if (modBName == "SETTINGS") return 1;
+                return (modAName < modBName) ? -1 : (modAName > modBName) ? 1 : 0;
+            });
+            return e(
+                "div", { style: rootStyle },
+                this.state.customSettings.length > 1 && e(
+                    "div", { style: { width: "100%" } },
+                    this.state.customSettings.map(
+                        (mod, index) => e(
+                            'div',
+                            {style: {border: '1px solid black'}},
+                            e(mod
+                             )
+                        )
+                    )
+                )
+            );
+        }
     }
-  }
 
-  const settingsContainer = document.createElement("div");
+    const settingsContainer = document.createElement("div");
 
-  document.getElementById("content").appendChild(settingsContainer);
+    document.getElementById("content").appendChild(settingsContainer);
 
-  ReactDOM.render(
-    e(CustomSettingsContainer),
-    settingsContainer
-  );
+    ReactDOM.render(
+        e(CustomSettingsContainer),
+        settingsContainer
+    );
 
+    /*
   class Settings extends React.Component {
     constructor (props) {
       super(props);
@@ -270,50 +261,22 @@ function main () {
       if (windowPreferences) {
         this.state = JSON.parse(windowPreferences);
       } else {
-        this.state = {
-          divWidth: 200,
-          divHeight: 150,
-        };
+        this.state = {};
       }
     }
 
     onApply () {
       localStorage.setItem("WINDOW_PREFERENCES", JSON.stringify(this.state));
-      Object.assign(settingsContainer.style, {
-        width: this.state.divWidth + "px",
-        height: this.state.divHeight + "px"
-      });
     }
 
     render () {
-      return e("div", null,
-        e('button', {onClick: () => this.onApply()}, 'Apply'),
-        e("div", {style: {marginBottom: '5px'}},
-          "Window Width ",
-          e("input", { style: { width: "3.3em" }, type: "number",
-            min: 200, max: 300, step: 1,
-            value: this.state.divWidth,
-            onChange: e => {
-              let w = parseFloat(e.target.value);
-              if (200 <= w && w <= 300) {
-                this.setState({ divWidth: w });
-              }
-            }
-          })
-        ),
-        e("div", {style: {marginBottom: '5px'}},
-          "Window Height ",
-          e("input", { style: { width: "3.3em" }, type: "number",
-            min: 150, max: 250, step: 1,
-            value: this.state.divHeight,
-            onChange: e => {
-              let h = parseFloat(e.target.value);
-              if (150 <= h && h <= 250) {
-                this.setState({ divHeight: h });
-              }
-            }
-          }))
-       )
+      return e(
+          "div",
+          null,
+          "Settings",
+          e('br'),
+          e('button', {onClick: () => this.onApply()}, 'Apply'),
+      )
     }
   }
 
@@ -323,15 +286,12 @@ function main () {
 
   if (windowPreferences) {
     let parsedPreferences = JSON.parse(windowPreferences);
-    Object.assign(settingsContainer.style, {
-      width: parsedPreferences.divWidth + "px",
-      height: parsedPreferences.divHeight + "px"
-    });
   }
+  */
 }
 
 if (window.store) {
-  main();
+    main();
 } else {
-  window.onAppReady = main;
+    window.onAppReady = main;
 }
