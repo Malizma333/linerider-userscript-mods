438d620214220824b9c9066129a2b21e806237dc
Malizma333 <malizma333@gmail.com>
2024-10-06 18:59:32 -0500

    update improved mod api

--- mods/line-rider-improved-api.user.js
+++ mods/line-rider-improved-api.user.js
@@ -4,15 +4,15 @@
 // @namespace    https://www.linerider.com/
 // @author       Malizma
 // @description  Container for linerider.com mods
-// @version      1.5.0
+// @version      1.7.2
 // @icon         https://www.linerider.com/favicon.ico
 
 // @match        https://www.linerider.com/*
 // @match        https://*.official-linerider.com/*
 // @match        https://*.surge.sh/*
 
-// @downloadURL  https://github.com/Malizma333/linerider-userscript-mods/raw/master/mods/line-rider-improved-api.user.js
-// @updateURL    https://github.com/Malizma333/linerider-userscript-mods/raw/master/mods/line-rider-improved-api.user.js
+// @downloadURL  https://github.com/Malizma333/linerider-userscript-mods/raw/master/line-rider-improved-api.user.js
+// @updateURL    https://github.com/Malizma333/linerider-userscript-mods/raw/master/line-rider-improved-api.user.js
 // @homepageURL  https://github.com/Malizma333/linerider-userscript-mods
 // @supportURL   https://github.com/Malizma333/linerider-userscript-mods/issues
 // @grant        none
@@ -31,6 +31,37 @@ const getActiveTool = state => state.selectedTool
 const getWindowFocused = state => state.views.Main
 const getPlayerRunning = state => state.player.running
 
+const defaultPreferences = {
+  settingsTop: '65vh',
+  settingsLeft: '78vw',
+  settingsWidth: '20vw',
+  settingsHeight: '25vh'
+}
+const preferencesKey = 'MOD_API_PREFERENCES'
+
+function loadPrefs () {
+  const storedPreferences = window.localStorage.getItem(preferencesKey)
+  if (storedPreferences) {
+    const nextPrefs = JSON.parse(storedPreferences)
+
+    Object.keys(defaultPreferences)
+      .filter(key => !Object.keys(nextPrefs).includes(key))
+      .forEach(key => {
+        nextPrefs[key] = defaultPreferences[key]
+      })
+    return nextPrefs
+  }
+
+  return defaultPreferences
+}
+
+function savePrefs (newPrefs) {
+  window.localStorage.setItem(
+    preferencesKey,
+    JSON.stringify(newPrefs)
+  )
+}
+
 function main () {
   window.V2 = window.V2 || window.store.getState().simulator.engine.engine.state.startPoint.constructor
 
@@ -44,13 +75,19 @@ function main () {
   let playerRunning = getPlayerRunning(store.getState())
   let windowFocused = getWindowFocused(store.getState())
 
+  const preferences = loadPrefs()
+  savePrefs(preferences)
+
   const rootStyle = {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'flex-start',
     textAlign: 'left',
     transition: 'opacity 225ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
-    width: '100%'
+    flex: 1,
+    width: '100%',
+    overflowY: 'auto',
+    overflowX: 'hidden'
   }
 
   const boxStyle = {
@@ -59,6 +96,47 @@ function main () {
     padding: 8,
     width: '100%'
   }
+
+  const settingsContainerStyle = {
+    position: 'fixed',
+    minWidth: '15vw',
+    maxWidth: '40vw',
+    minHeight: '15vh',
+    maxHeight: '45vh',
+    border: '3px solid black',
+    backgroundColor: '#ffffff',
+    resize: 'both',
+    overflow: 'hidden',
+    opacity: 0,
+    pointerEvents: 'none'
+  }
+
+  const toolContainerStyle = {
+    position: 'fixed',
+    width: '200px',
+    height: '16%',
+    overflowY: 'auto',
+    overflowX: 'hidden',
+    top: '8px',
+    right: '250px',
+    border: '1px solid black',
+    backgroundColor: '#ffffff',
+    pointerEvents: 'none',
+    opacity: 0
+  }
+
+  const headerStyle = {
+    cursor: 'move',
+    minHeight: '3ch',
+    backgroundColor: '#aaaaaa',
+    zIndex: '10',
+    borderBottom: '3px solid black'
+  }
+
+  settingsContainerStyle.top = preferences.settingsTop
+  settingsContainerStyle.left = preferences.settingsLeft
+  settingsContainerStyle.width = preferences.settingsWidth
+  settingsContainerStyle.height = preferences.settingsHeight
 
   store.subscribe(() => {
     playerRunning = getPlayerRunning(store.getState())
@@ -114,19 +192,7 @@ function main () {
 
         if (!containerAssigned) {
           containerAssigned = true
-          Object.assign(toolContainer.style, {
-            position: 'fixed',
-            width: '200px',
-            height: '16%',
-            overflowY: 'auto',
-            overflowX: 'hidden',
-            top: '8px',
-            right: '250px',
-            border: '1px solid black',
-            backgroundColor: '#ffffff',
-            opacity: 0,
-            pointerEvents: 'none'
-          })
+          Object.assign(toolContainer.style, toolContainerStyle)
         }
       }
     }
@@ -176,6 +242,8 @@ function main () {
     toolContainer
   )
 
+  const dragProps = { lastX: 0, lastY: 0 }
+
   class CustomSettingsContainer extends React.Component {
     constructor () {
       super()
@@ -196,44 +264,105 @@ function main () {
 
         if (!containerAssigned) {
           containerAssigned = true
-          Object.assign(settingsContainer.style, {
-            position: 'fixed',
-            width: '20vw',
-            height: '25vh',
-            overflowY: 'auto',
-            overflowX: 'hidden',
-            bottom: '15%',
-            right: '8px',
-            border: '1px solid black',
-            backgroundColor: '#ffffff',
-            opacity: 0,
-            pointerEvents: 'none'
-          })
+          Object.assign(settingsContainer.style, settingsContainerStyle)
         }
       }
 
       if (typeof window.onCustomToolsApiReady === 'function') {
         window.onCustomToolsApiReady()
       }
+
+      window.addEventListener('resize', this.updateDimensions)
+    }
+
+    updateDimensions () {
+      if (settingsContainer.offsetLeft < 0) {
+        settingsContainer.style.left = `${0}vw`
+        preferences.settingsLeft = `${0}vw`
+      }
+
+      if (settingsContainer.offsetLeft > window.innerWidth - settingsContainer.offsetWidth) {
+        settingsContainer.style.left = `${100 * (window.innerWidth - settingsContainer.offsetWidth) / window.innerWidth}vw`
+        preferences.settingsLeft = `${100 * (window.innerWidth - settingsContainer.offsetWidth) / window.innerWidth}vw`
+      }
+
+      if (settingsContainer.offsetTop < 0) {
+        settingsContainer.style.top = `${0}vh`
+        preferences.settingsTop = `${0}vh`
+      }
+
+      if (settingsContainer.offsetTop > window.innerHeight - settingsContainer.offsetHeight) {
+        settingsContainer.style.top = `${100 * (window.innerHeight - settingsContainer.offsetHeight) / window.innerHeight}vh`
+        preferences.settingsTop = `${100 * (window.innerHeight - settingsContainer.offsetHeight) / window.innerHeight}vh`
+      }
+
+      savePrefs(preferences)
+    }
+
+    onStartDrag (e) {
+      e = e || window.event
+      e.preventDefault()
+
+      dragProps.lastX = e.clientX
+      dragProps.lastY = e.clientY
+
+      document.onmousemove = this.onDrag
+      document.onmouseup = this.onCloseDrag
+    }
+
+    onDrag (e) {
+      e = e || window.event
+      e.preventDefault()
+
+      const newX = dragProps.lastX - e.clientX
+      const newY = dragProps.lastY - e.clientY
+      dragProps.lastX = e.clientX
+      dragProps.lastY = e.clientY
+
+      const nextOffsetX = settingsContainer.offsetLeft - newX
+      const nextOffsetY = settingsContainer.offsetTop - newY
+
+      if (nextOffsetX >= 0 && nextOffsetX <= window.innerWidth - settingsContainer.offsetWidth &&
+             nextOffsetY >= 0 && nextOffsetY <= window.innerHeight - settingsContainer.offsetHeight) {
+        settingsContainer.style.left = `${100 * (settingsContainer.offsetLeft - newX) / window.innerWidth}vw`
+        settingsContainer.style.top = `${100 * (settingsContainer.offsetTop - newY) / window.innerHeight}vh`
+        preferences.settingsLeft = `${100 * (settingsContainer.offsetLeft - newX) / window.innerWidth}vw`
+        preferences.settingsTop = `${100 * (settingsContainer.offsetTop - newY) / window.innerHeight}vh`
+      }
+
+      savePrefs(preferences)
+    }
+
+    onCloseDrag (e) {
+      document.onmouseup = null
+      document.onmousemove = null
     }
 
     render () {
       this.state.customSettings.sort(function (modA, modB) {
         const modAName = modA.name.toUpperCase()
         const modBName = modB.name.toUpperCase()
-        if (modAName == 'SETTINGS') return -1
-        if (modBName == 'SETTINGS') return 1
         return (modAName < modBName) ? -1 : (modAName > modBName) ? 1 : 0
       })
+
       return e(
-        'div', { style: rootStyle },
-        this.state.customSettings.length > 1 && e(
-          'div', { style: { width: '100%' } },
-          this.state.customSettings.map(
-            (mod, index) => e(
-              'div',
-              { style: { border: '1px solid black' } },
-              e(mod
+        'div',
+        { style: { display: 'flex', height: '100%', width: '100%', flexDirection: 'column' } },
+        e(
+          'div',
+          { style: headerStyle, onMouseDown: (e) => this.onStartDrag(e) }
+        ),
+        e(
+          'div',
+          { style: rootStyle },
+          this.state.customSettings.length > 0 && e(
+            'div',
+            { style: { width: '100%' } },
+            this.state.customSettings.map(
+              (mod, index) => e(
+                'div',
+                { style: { borderTop: index > 0 ? '2px solid black' : null } },
+                e(mod)
               )
             )
           )
@@ -251,47 +380,21 @@ function main () {
     settingsContainer
   )
 
-  /*
-  class Settings extends React.Component {
-    constructor (props) {
-      super(props);
+  const resizeObserver = new window.ResizeObserver(() => {
+    preferences.settingsWidth = `${100 * settingsContainer.offsetWidth / window.innerWidth}vw`
+    preferences.settingsHeight = `${100 * settingsContainer.offsetHeight / window.innerHeight}vh`
+    savePrefs(preferences)
+  })
 
-      let windowPreferences = localStorage.getItem("windowPreferences");
-
-      if (windowPreferences) {
-        this.state = JSON.parse(windowPreferences);
-      } else {
-        this.state = {};
-      }
-    }
-
-    onApply () {
-      localStorage.setItem("WINDOW_PREFERENCES", JSON.stringify(this.state));
-    }
-
-    render () {
-      return e(
-          "div",
-          null,
-          "Settings",
-          e('br'),
-          e('button', {onClick: () => this.onApply()}, 'Apply'),
-      )
-    }
-  }
-
-  window.registerCustomSetting(Settings);
-
-  let windowPreferences = localStorage.getItem("WINDOW_PREFERENCES");
-
-  if (windowPreferences) {
-    let parsedPreferences = JSON.parse(windowPreferences);
-  }
-  */
+  resizeObserver.observe(settingsContainer)
 }
 
 if (window.store) {
   main()
 } else {
-  window.onAppReady = main
+  const prevInit = window.onAppReady
+  window.onAppReady = () => {
+    if (prevInit) prevInit()
+    main()
+  }
 }
