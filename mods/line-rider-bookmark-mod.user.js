771fc14b91ab431b1b47411157fbfb603fe1faf0
Tobias Bessler <malizma333@gmail.com>
2024-10-06 21:24:41 -0500

    Update line-rider-bookmark-mod.user.js
    restyled

--- mods/line-rider-bookmark-mod.user.js
+++ mods/line-rider-bookmark-mod.user.js
@@ -4,7 +4,7 @@
 // @namespace    https://www.linerider.com/
 // @author       Malizma
 // @description  Allows you to make bookmarks that act similar to flags but there's multiple
-// @version      1.3.1
+// @version      1.3.2
 // @icon         https://www.linerider.com/favicon.ico
 
 // @match        https://www.linerider.com/*
@@ -27,7 +27,7 @@ function main () {
     React,
     store
   } = window
-  const c = React.createElement
+  const e = React.createElement
 
   class BookmarkModComponent extends React.Component {
     constructor (props) {
@@ -35,6 +35,7 @@ function main () {
 
       this.state = {
         active: false,
+        showBpmGen: false,
         indexView: false,
         bpm: 60,
         start: 0,
@@ -93,6 +94,14 @@ function main () {
       window.localStorage.setItem('BOOKMARK_MOD_TIMESTAMPS', JSON.stringify(nextState.timestamps))
     }
 
+    onAddTimestamp () {
+      const timestamps = this.state.timestamps
+      const currentIndex = store.getState().player.index
+      const currentTimestamp = this.convertIndexToTime(currentIndex)
+      timestamps.unshift(currentTimestamp)
+      this.setState({ timestamps })
+    }
+
     convertIndexToTime (index) {
       const time = [
         Math.floor(index / 2400),
@@ -105,6 +114,16 @@ function main () {
     convertTimeToIndex (time) {
       const index = time[0] * 2400 + time[1] * 40 + time[2]
       return index
+    }
+    
+    renderSection (key, title) {
+      return e('div', null,
+        e('button',
+          { id: key, style: { background: 'none', border: 'none' }, onClick: () => this.setState({ [key]: !this.state[key] }) },
+          this.state[key] ? '▲' : '▼'
+        ),
+        e('label', { for: key }, title)
+      )
     }
 
     renderFramePicker (index) {
@@ -119,8 +138,9 @@ function main () {
         }
       }
 
-      return c('div', null,
-        c('input', { style: { width: '6em' }, ...props })
+      return e(
+        'div', null,
+        e('input', { style: { width: '6em' }, ...props })
       )
     }
 
@@ -132,9 +152,10 @@ function main () {
         onChange: e => this.setState({ [key]: parseInt(e.target.value) })
       }
 
-      return c('div', null,
-        title,
-        c('input', { style: { width: '4em' }, ...props })
+      return e(
+        'div', null,
+        title + ' ',
+        e('input', { style: { width: '4em' }, ...props })
       )
     }
 
@@ -150,96 +171,110 @@ function main () {
         }
       }
 
-      return c('div', null,
-        c('input', { style: { width: '2em' }, ...props })
+      return e(
+        'div', null,
+        e('input', { style: { width: '2em' }, ...props })
       )
     }
 
     renderBPMGenerator () {
       const maxIndex = window.store.getState().player.maxIndex
-      return c('div', null,
-        'Generating BPM Markers',
-        c('button', { onClick: this.onGenerateBPM.bind(this) }, 'Generate'),
-        c('div', { style: { display: 'flex', flexDirection: 'row' } },
-          this.renderNumberPicker('start', 'Start', { min: 0, max: 1000, step: 1 }),
-          this.renderNumberPicker('end', 'End', { min: 0, max: maxIndex, step: 1 }),
-          this.renderNumberPicker('bpm', 'BPM', { min: 0, max: maxIndex, step: 1 })
-        ),
-        this.renderNumberPicker('beats', 'Beat Count (1/x) ', { min: 1, max: 256, step: 1 })
+      return e(
+        'div', null,
+        this.renderNumberPicker('beats', 'Beat Count (1/x) ', { min: 1, max: 256, step: 1 }),
+        this.renderNumberPicker('start', 'Start', { min: 0, max: 1000, step: 1 }),
+        this.renderNumberPicker('end', 'End', { min: 0, max: maxIndex, step: 1 }),
+        this.renderNumberPicker('bpm', 'BPM', { min: 0, max: maxIndex, step: 1 }),
+        e('button', { onClick: this.onGenerateBPM.bind(this) }, 'Generate')
       )
     }
 
     renderTimeStamp (index) {
-      return c('div', null,
-        c('input', {
-          style: { width: '8em' },
-          type: 'text',
-          value: this.state.timestamps[index][3],
-          onChange: (e) => {
-            const timestamps = this.state.timestamps
-            timestamps[index][3] = e.target.value
-            this.setState({ timestamps })
-          }
-        }),
-        c('div', { style: { display: 'flex', flexDirection: 'row' } },
-          c('button', {
-            onClick: () => {
-              const targetTimestamp = this.state.timestamps[index]
-              const targetFrame = this.convertTimeToIndex(targetTimestamp)
-              store.dispatch({ type: 'SET_PLAYER_INDEX', payload: targetFrame })
-            }
-          }, '>'),
-          this.state.indexView
-            ? c('div', null,
-              this.renderFramePicker(index)
-            )
-            : c('div', { style: { display: 'flex', flexDirection: 'row' } },
-              this.renderTimePicker(index, 0, { min: 0, max: 59 }), ':',
-              this.renderTimePicker(index, 1, { min: 0, max: 59 }), ':',
-              this.renderTimePicker(index, 2, { min: 0, max: 39 })
-            ),
-          c('button', {
-            onClick: () => {
-              if (!window.confirm('Remove bookmark?')) return
+      return e(
+        'div', null,
+        e(
+          'input', {
+            style: { width: '8em' },
+            type: 'text',
+            value: this.state.timestamps[index][3],
+            onChange: (e) => {
               const timestamps = this.state.timestamps
-              timestamps.splice(index, 1)
+              timestamps[index][3] = e.target.value
               this.setState({ timestamps })
             }
-          }, '-')
+          }),
+        e(
+          'div', { style: { display: 'flex', flexDirection: 'row' } },
+          e(
+            'button', {
+              onClick: () => {
+                const targetTimestamp = this.state.timestamps[index]
+                const targetFrame = this.convertTimeToIndex(targetTimestamp)
+                store.dispatch({ type: 'SET_PLAYER_INDEX', payload: targetFrame })
+              }
+            }, '>'),
+          this.state.indexView
+          ? e(
+            'div', null,
+            this.renderFramePicker(index)
+          )
+          : e(
+            'div', { style: { display: 'flex', flexDirection: 'row' } },
+            this.renderTimePicker(index, 0, { min: 0, max: 59 }), ':',
+            this.renderTimePicker(index, 1, { min: 0, max: 59 }), ':',
+            this.renderTimePicker(index, 2, { min: 0, max: 39 })
+          ),
+          e(
+            'button', {
+              onClick: () => {
+                if (!window.confirm('Remove bookmark?')) return
+                const timestamps = this.state.timestamps
+                timestamps.splice(index, 1)
+                this.setState({ timestamps })
+              }
+            }, '-')
         )
       )
     }
 
     render () {
-      return c('div', null,
-        c('button', {
-          style: { backgroundColor: this.state.active ? 'lightblue' : null },
-          onClick: this.onActivate.bind(this)
-        }, 'Bookmark Mod'
-        ),
-        this.state.active && c('div', { style: { height: '20vh', border: '1px solid black', overflowY: 'auto', overflowX: 'hidden' } },
-          c('button', {
-            onClick: () => {
-              const timestamps = this.state.timestamps
-              const currentIndex = store.getState().player.index
-              const currentTimestamp = this.convertIndexToTime(currentIndex)
-              timestamps.unshift(currentTimestamp)
-              this.setState({ timestamps })
-            }
-          }, '+'),
-          c('button', {
-            onClick: () => {
-              if (!window.confirm('Remove all bookmarks?')) return
-              this.setState({ timestamps: [] })
-            }
-          }, 'X'),
-          c('button', { onClick: this.onIndexView.bind(this) },
+      return e(
+        'div', null,
+        this.state.active && e(
+          'div',
+          null,
+          e(
+            'button', {
+              onClick: () => this.onAddTimestamp()
+            }, '+'),
+          e(
+            'button', {
+              onClick: () => {
+                if (!window.confirm('Remove all bookmarks?')) return
+                this.setState({ timestamps: [] })
+              }
+            }, 'X'),
+          e(
+            'button', { onClick: this.onIndexView.bind(this) },
             this.state.indexView ? 'Show Times' : 'Show Indices'
           ),
-          this.state.timestamps.map((timestamp, index) => {
-            return c('div', { key: index }, this.renderTimeStamp(index))
-          }),
-          this.renderBPMGenerator()
+          e(
+            'div',
+            { style: { width: '100%', height: '10vh', overflowY: 'auto', border: '1px solid black' } },
+            this.state.timestamps.map((timestamp, index) => {
+              return e('div', { key: index }, this.renderTimeStamp(index))
+            })
+          ),
+          e('hr'),
+          this.renderSection('showBpmGen', 'Generate BPM Markers'),
+          this.state.showBpmGen && this.renderBPMGenerator()
+        ),
+        e(
+          'button', {
+            style: { backgroundColor: this.state.active ? 'lightblue' : null },
+            onClick: this.onActivate.bind(this)
+          },
+          'Bookmark Mod'
         )
       )
     }
