500c76c073cff05352e0021cea64024a78d79f88
Malizma333 <malizma333@gmail.com>
2024-12-21 11:22:00 -0600

    fixed platformer??

--- console-scripts/platformer-controller.js
+++ console-scripts/platformer-controller.js
@@ -1,5 +1,6 @@
 (function() {
   let keyPressed = { a: false, d: false, r: false }
+  let count = 0
 
   window.addEventListener("keydown", (e) => {
     for (const key of Object.keys(keyPressed)) {
@@ -19,10 +20,7 @@
 
   const accelForce = 0.0625
 
-  store.getState().camera.playbackFollower._frames.length = 0
-  store.getState().simulator.engine.engine._computed._frames.length = 1
-
-  Object.defineProperty(window.$ENGINE_PARAMS, "gravity", { get () {
+  Object.defineProperty(window.$ENGINE_PARAMS, "gravity", { get() {
     const cFrames = store.getState().simulator.engine.engine._computed._frames
     const rider = cFrames[cFrames.length-1].snapshot.entities[0].entities[0]
     const sledAngle = Math.atan2(
@@ -34,9 +32,14 @@
 
     if (keyPressed.r) {
       keyPressed.r = false
-      // Gravity api not working yet
-      // store.getState().camera.playbackFollower._frames.length = 0
-      // store.getState().simulator.engine.engine._computed._frames.length = 1
+      count += 1
+      store.dispatch({ type: "STOP_PLAYER" })
+      window.requestAnimationFrame(() => {
+        store.getState().camera.playbackFollower._frames.length = 0
+        const engineComputed = store.getState().simulator.engine.engine._computed
+        engineComputed._frames = [engineComputed._getInitialFrame()]
+        store.dispatch({ type: "START_PLAYER" })
+      })
     }
 
     if (keyPressed.a != keyPressed.d) {
@@ -49,5 +52,5 @@
     }
 
     return gravity
-  } })
+  }})
 })()
