b6fc6c62b48aba8215a5197a6a32b14b03ddba3b
Malizma333 <malizma333@gmail.com>
2024-12-21 13:33:08 -0600

    capped speed and omnidirectional movement

--- console-scripts/platformer-controller.js
+++ console-scripts/platformer-controller.js
@@ -1,6 +1,14 @@
 (function() {
+  const thrustMag = 0.125
+  const maxSpeed = 10
+  const maxSpeedScaled = maxSpeed * maxSpeed * 100
   let keyPressed = { a: false, d: false, r: false }
-  let count = 0
+
+  store.dispatch({ type: "SET_PLAYER_INDEX", payload: 0 })
+  window.requestAnimationFrame(() => {
+    store.getState().camera.playbackFollower._frames.length = 0
+    store.getState().simulator.engine.engine._computed._frames.length = 1
+  })
 
   window.addEventListener("keydown", (e) => {
     for (const key of Object.keys(keyPressed)) {
@@ -18,37 +26,50 @@
     }
   }, false)
 
-  const accelForce = 0.0625
+  function getSpeedSquared(rider, prevRider) {
+    let x = 0, y = 0
+
+    rider.filter(p => p.type === 'CollisionPoint').forEach(p => {
+      x += p.pos.x
+      y += p.pos.y
+    })
+
+    prevRider.filter(p => p.type === 'CollisionPoint').forEach(p => {
+      x -= p.pos.x
+      y -= p.pos.y
+    })
+
+    return [x * x + y * y, Math.atan2(-y, x)]
+  }
 
   Object.defineProperty(window.$ENGINE_PARAMS, "gravity", { get() {
     const cFrames = store.getState().simulator.engine.engine._computed._frames
-    const rider = cFrames[cFrames.length-1].snapshot.entities[0].entities[0]
-    const sledAngle = Math.atan2(
-      rider.points[0].pos.y - rider.points[3].pos.y,
-      rider.points[0].pos.x - rider.points[3].pos.x
-    )
+    const curIndex = Math.max(1, Math.min(cFrames.length, Math.floor(store.getState().player.index)))
+    const rider = cFrames[curIndex - 1].snapshot.entities[0].entities[0]
 
-    let gravity = { x: 0, y: 0.175 }
+    let gravity = { x: 0, y: 0 }
 
     if (keyPressed.r) {
       keyPressed.r = false
-      count += 1
-      store.dispatch({ type: "STOP_PLAYER" })
+      store.dispatch({ type: "SET_PLAYER_INDEX", payload: 0 })
       window.requestAnimationFrame(() => {
         store.getState().camera.playbackFollower._frames.length = 0
-        const engineComputed = store.getState().simulator.engine.engine._computed
-        engineComputed._frames = [engineComputed._getInitialFrame()]
-        store.dispatch({ type: "START_PLAYER" })
+        store.getState().simulator.engine.engine._computed._frames.length = 1
       })
     }
 
-    if (keyPressed.a != keyPressed.d) {
-      if (keyPressed.a) {
-        gravity.x += accelForce * Math.cos(sledAngle)
-      }
-      if (keyPressed.d) {
-        gravity.x -= accelForce * Math.cos(sledAngle)
-      }
+    if (keyPressed.a !== keyPressed.d) {
+      const prevRider = cFrames[Math.max(0, curIndex - 2)].snapshot.entities[0].entities[0]
+      const [speedMag, speedAngle] = getSpeedSquared(rider.points, prevRider.points)
+      const dx = rider.points[3].pos.x - rider.points[0].pos.x
+      const dy = rider.points[0].pos.y - rider.points[3].pos.y
+      const thrustAngle = Math.atan2(dy, dx) + (keyPressed.a ? Math.PI : 0)
+      const percentLeft = Math.max(0, 1 - speedMag / maxSpeedScaled)
+      const angleDiff = Math.abs(thrustAngle - speedAngle)
+      const angleDiffMod = angleDiff > Math.PI * 2 ? angleDiff - Math.PI * 2 : angleDiff
+      const shouldScale = angleDiffMod < Math.PI / 2
+      gravity.x += thrustMag * (shouldScale ? percentLeft : 1) * Math.cos(thrustAngle)
+      gravity.y -= thrustMag * (shouldScale ? percentLeft : 1) * Math.sin(thrustAngle)
     }
 
     return gravity
