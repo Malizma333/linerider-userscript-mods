3cc5a3f7c64ee50286a7d353a5cc3579a0218227
Tobias Bessler <malizma333@gmail.com>
2024-12-23 13:27:10 -0600

    Update line-rider-command-editor.user.js

--- mods/line-rider-command-editor.user.js
+++ mods/line-rider-command-editor.user.js
@@ -4,7 +4,7 @@ window.CMD_EDITOR_DEBUG=false
 // @author       Malizma
 // @description  Adds UI to API commands in linerider.com
 // @namespace    https://www.linerider.com/
-// @version      372.0
+// @version      375.0
 // @icon         https://www.linerider.com/favicon.ico
 // @match        https://www.linerider.com/*
 // @match        https://*.official-linerider.com/*
