3e465529b343c60d02f8774b67d4efc4a911b7ca
Tobias Bessler <malizma333@gmail.com>
2025-02-13 08:43:39 -0600

    Update line-rider-command-editor.user.js

--- mods/line-rider-command-editor.user.js
+++ mods/line-rider-command-editor.user.js
@@ -4,7 +4,7 @@ window.CMD_EDITOR_DEBUG=false
 // @author       Malizma
 // @description  Adds UI to API commands in linerider.com
 // @namespace    https://www.linerider.com/
-// @version      453.0
+// @version      457.0
 // @icon         https://www.linerider.com/favicon.ico
 // @match        https://www.linerider.com/*
 // @match        https://*.official-linerider.com/*
@@ -12,8 +12,8 @@ window.CMD_EDITOR_DEBUG=false
 // @match        https://*.surge.sh/*
 // @grant        none
 // @require      https://github.com/Malizma333/line-rider-command-editor-userscript/raw/master/command-editor.min.js
-// @downloadURL  https://github.com/Malizma333/linerider-userscript-mods/raw/master/mods/line-rider-bookmark-mod.user.js
-// @updateURL    https://github.com/Malizma333/linerider-userscript-mods/raw/master/mods/line-rider-bookmark-mod.user.js
+// @downloadURL  https://github.com/Malizma333/linerider-userscript-mods/raw/master/mods/line-rider-command-editor.user.js
+// @updateURL    https://github.com/Malizma333/linerider-userscript-mods/raw/master/mods/line-rider-command-editor.user.js
 // @homepageURL  https://github.com/Malizma333/linerider-userscript-mods
 // @supportURL   https://github.com/Malizma333/linerider-userscript-mods/issues
 // ==/UserScript==
