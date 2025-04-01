273b22d583700e58b7313e294459c687d261878d
Tobias Bessler <malizma333@gmail.com>
2025-03-31 21:30:49 -0500

    Create line-rider-command-editor-staging.user.js

---
+++ mods/line-rider-command-editor-staging.user.js
@@ -0,0 +1,19 @@
+window.CMD_EDITOR_DEBUG=false;
+// ==UserScript==
+// @name         Command Editor Staging
+// @author       Malizma
+// @description  Adds UI to API commands in linerider.com
+// @namespace    https://www.linerider.com/
+// @version      490.0
+// @icon         https://www.linerider.com/favicon.ico
+// @match        https://www.linerider.com/*
+// @match        https://*.official-linerider.com/*
+// @match        http://localhost:*/*
+// @match        https://*.surge.sh/*
+// @grant        none
+// @require      https://raw.githubusercontent.com/Malizma333/line-rider-command-editor-userscript/refs/heads/staging/command-editor.min.js
+// @downloadURL  https://github.com/Malizma333/linerider-userscript-mods/raw/master/mods/line-rider-command-editor-staging.user.js
+// @updateURL    https://github.com/Malizma333/linerider-userscript-mods/raw/master/mods/line-rider-command-editor-staging.user.js
+// @homepageURL  https://github.com/Malizma333/linerider-userscript-mods
+// @supportURL   https://github.com/Malizma333/linerider-userscript-mods/issues
+// ==/UserScript==
